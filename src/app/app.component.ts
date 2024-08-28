f26dda7923c05bbe2cb7bd574e4d8ec70283d5ac
Miguel angel Bissuett lira <miguelangel.292020@gmail.com>
2024-08-28 04:06:22 -0600

    Upload project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,24 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'NOTIFICATION_API';
+
+  public notification = () => {
+    Notification.requestPermission().then((result) => {
+      if (result === 'granted') {
+        this.popupNotif();
+      } else {
+        console.log('Permission denied');
+      }
+    })
+  }
+
+  public popupNotif = () => {
+    const notification = new Notification('New notification', {
+      body: 'This a default notification message'
+    });
+
+    notification.onclick = () => {
+      console.log('Notification clicked!');
+    }
+  }
 }
